dd7dd19f40d5f62d778031a90cc985695c23fb4e
pavelnosarev <pavelnosarev@outlook.com>
2023-04-17 11:13:23 -0400

    first commit

---
+++ src/main/java/com/pn/WorkingWithStrings.java
@@ -0,0 +1,15 @@
+package com.pn;
+
+public class WorkingWithStrings {
+
+    public static void main (String [] args) {
+        System.out.println(" My super power is : Time Manipulation.");
+
+
+
+    }
+
+
+
+
+}
